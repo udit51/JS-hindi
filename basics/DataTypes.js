e2442793bc72ee5f797b922a315ff62f93893c48
Udit Pratap Singh <udsingha5100@gmail.com>
2025-01-23 18:02:16 +0000

    DataTypes , vars and conversions

---
+++ basics/DataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+//alert (3) //we are using node js not browser that is why throwing error
+
+
+let name ="udit"  // String
+let age = 18      // number
+let isloggedIn = false  //bool
+//bigint 
+//number 
+//string -> prefer in double quote  (" ")
+//null -> also a datatype but a standalone value 
+//undefined -> empty (no value assigned)-> different from NULL
+//symbol -> when we talk about unique value
+
+
+//object 
+
+console.log(typeof "udit");
+console.log(typeof null)
+
