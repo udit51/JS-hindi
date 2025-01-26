092c9c1ebb61547b0ab6361113f2bb8c19c0405d
Udit Pratap Singh <udsingha5100@gmail.com>
2025-01-26 17:56:23 +0000

    Data Types Summary

---
+++ basics/DataTypes2.js
@@ -0,0 +1,12 @@
+//Primitive Data Types -> all are call by value i.e. they are stored in stack memory and they are immutable here we do not get direct access to memory location we just get the copy of the value and only allowed to makes changes there
+
+// String , Number , Boolean , BigInt -> big values , Symbol , Undefined , Null
+
+
+
+//Reference data types or non primitive -> Objects stored in heap memory and we get the direct access to memory location and we can make changes there
+
+//Array , Object Literals , Functions or Methods , Dates , Anything else
+
+
+// JavaScript is a loosely typed language, which means that you don't have to declare the data type of a variable when you declare it. JavaScript automatically converts the variable to the correct data type, depending on its value. For example, if you declare a variable and assign it a string value, JavaScript will automatically convert the variable to a string data type.
