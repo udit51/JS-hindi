448122327cbd36e4b0927c51bd0dcc31a47bdf01
Udit Pratap Singh <udsingha5100@gmail.com>
2025-02-14 19:21:22 +0000

    Functions and scope

---
+++ 03_Basics/Arrow.js
@@ -0,0 +1,58 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username); ->   we can not use this keyword in function expression 
+// }
+
+
+
+//Arrow function
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);   // -> this keyword will not work in arrow function we will get empty object
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
